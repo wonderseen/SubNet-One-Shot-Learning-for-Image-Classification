d850604f8854db90da62a45ba099d272a8e9533e
WonderSeen <247226176@qq.com>
2018-05-11 18:16:15 +0800

    Update train_gray.py

--- SubNet/train_gray.py
+++ SubNet/train_gray.py
@@ -33,11 +33,11 @@ import Draw_ROC_iteration
 if __name__ == '__main__':
     model_addr = './model/classification.ckpt-'
     step = 0
-    RGB_net = Network2.CL_Network(start_step=step)
-    RGB_net.train(continueflag=True, model_addr=model_addr+str(step),debug_labels=False)
-    #RGB_net.test_single_threshold(model_addr=model_addr+str(step), test_num=100, test_step=100, threshold=0.5, debug_labels=False)
-    #RGB_net.get_ROC(model_addr=model_addr+str(step), test_num=1000, saveResult=True, saveTXTName=str(step)+'.txt')
-    #RGB_net.test(model_addr=model_addr+str(step), test_num=100, debug_labels=False)
-    #RGB_net.test_distance(model_addr=model_addr+str(step),test_num=100,test_step=50)
+    GRAY_net = Network2.CL_Network(start_step=step)
+    GRAY_net.train(continueflag=True, model_addr=model_addr+str(step),debug_labels=False)
+    #GRAY_net.test_single_threshold(model_addr=model_addr+str(step), test_num=100, test_step=100, threshold=0.5, debug_labels=False)
+    #GRAY_net.get_ROC(model_addr=model_addr+str(step), test_num=1000, saveResult=True, saveTXTName=str(step)+'.txt')
+    #GRAY_net.test(model_addr=model_addr+str(step), test_num=100, debug_labels=False)
+    #GRAY_net.test_distance(model_addr=model_addr+str(step),test_num=100,test_step=50)
     #Draw_ROC_iteration.compuse(txtName = ["1","100","200","300","400","500","600"])
 
